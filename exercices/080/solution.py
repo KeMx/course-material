a5643c648c5967b50da2567590811ae246cf78e5
KeMx <git@kemx.de>
2014-09-22 19:42:58 +0200

    solut 080

---
+++ exercices/080/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 19:08:15 2014
+
+@author: ne
+"""
+
+
+ab = "abcdefghijklmnopqrstuvwxyz"
+
+for i in range(len(ab)):
+    for j in range(len(ab)):
+       if i < j:
+           print(ab[i] + ab[j])
