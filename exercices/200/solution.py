166f87cddafd242fa88c8cf214e1140ebc958e3e
KeMx <git@kemx.de>
2014-09-22 19:42:59 +0200

    solut 200

---
+++ exercices/200/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 19:13:15 2014
+
+@author: ne
+"""
+
+def is_prime(num):
+    res = True
+    for i in range(2,num):
+        if num % i == 0:
+            res = False
+    print (res)
