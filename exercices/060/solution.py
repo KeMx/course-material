37d8eeb4908983bba9a25d5e0da165ec36a01896
KeMx <KeMx@users.noreply.github.com>
2014-09-22 22:28:33 +0200

    Update solution.py

--- exercices/060/solution.py
+++ exercices/060/solution.py
@@ -7,6 +7,6 @@ Created on Mon Sep 22 19:03:10 2014
 
 ab = "abcdefghijklnmopqrstuvwxyz"
 
-for i in range(len(ab)):
-    for j in range(len(ab)):
-        print(ab[i] + ab[j])
+for i in ab:
+    for j in ab:
+        print(i + j)
