ffb74141c16732f5fddf90b06417c7b4abde407c
KeMx <git@kemx.de>
2014-09-22 21:28:58 +0200

    solut 200

--- exercices/200/solution.py
+++ exercices/200/solution.py
@@ -5,9 +5,10 @@ Created on Mon Sep 22 19:13:15 2014
 @author: ne
 """
 
+
 def is_prime(num):
     res = True
-    for i in range(2,num):
+    for i in range(2, num):
         if num % i == 0:
             res = False
-    print (res)
+    print(res)
